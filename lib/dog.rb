61b502e700dfa9765180596ede3f352958e6aaf6
Luis Escobar <lae311@stern.nyu.edu>
2021-11-22 19:42:37 -0500

    commit tree b64d560c69c08164ba25a8b178ad9021a607a345

---
+++ lib/dog.rb
@@ -0,0 +1,10 @@
+class Dog
+    # Im constructing an instance of dog where I am giving it an instance method of bark which returns "woof"
+    def bark
+        puts "Woof!"
+    end
+
+    def sit
+        puts "The Dog is sitting"
+    end
+end
